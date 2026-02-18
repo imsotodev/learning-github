410aca649bae2491dc6801611df4ee994892c6a1
imsotodev <jesus.sotopaguay@livocompany.com>
2026-02-17 22:32:49 -0500

    feat(example): :sparkles: ejemplo de app router para gh con kike y el nv de armando

---
+++ src/app/kike/page.tsx
@@ -0,0 +1,7 @@
+export default function KikePage() {
+  return (
+    <div>
+      <h1>Kike</h1>
+    </div>
+  );
+}
---
+++ src/app/soto/page.tsx
@@ -0,0 +1,10 @@
+export default function SotoPage() {
+  const name = "Soto";
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">{name}</h1>
+      <button className="btn btn-primary">Click me</button>
+      <button className="btn btn-secondary">Click me</button>
+    </div>
+  );
+}
